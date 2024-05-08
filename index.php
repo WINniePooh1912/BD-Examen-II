5333ba813e00aef31249b0592fa3b88ac7a143a2
eri <a22310383@ceti.mx>
2024-05-08 14:38:45 -0600

    css floreria

--- index.php
+++ index.php
@@ -29,14 +29,19 @@ include "conn.php";
 			if(!isset($_SESSION['id'])) {
 				echo "<h3>Bienvenido, huésped, usted aún no ha iniciado sesión </h3>";
 				echo "<p class='parrafo'>Inicia sesión, por favorcito</p>";
+				echo "<p class='parrafo'> </p>";
 			} else {
 				$id = $_SESSION['id'];
 			$usr = mysqli_fetch_array(mysqli_query($con, "SELECT * FROM usuarios WHERE id = '$id'"));
 
 			$_SESSION['usuario'] = $usr['usuario'];
 
-			echo "<h3>Bienvenida personita con el usuario: <span>" .$_SESSION['usuario'] ."</span></h3>";
-			echo "<p class='parrafo'>Somos una floreria nacida en Guadalajara, Jalisco, México <p>";
+			echo "<h3>¡Bienvenido <span>" .$_SESSION['usuario'] ."</span>!</h3>";
+			echo "<p class='parrafo'>Tu florería en Guadalajara </p>";
+			// echo "<p class='parrafo'>¡Bienvenido a nuestra florería en Guadalajara! Nuestra pasión por las flores va más allá del simple arte de crear arreglos: cada ramo es un testimonio de una historia única. Un día, una joven llamada Ana vino a nuestra tienda buscando flores para celebrar el aniversario de sus abuelos. Nos contó cómo, hace 50 años, su abuelo solía llevar a su abuela un ramo de girasoles cada semana. Nos inspiró tanto la ternura de su historia que creamos un arreglo especial con girasoles, margaritas y lirios, simbolizando el amor duradero y la belleza de los recuerdos compartidos. Desde ese momento, nos comprometimos a ser parte de las historias más bellas y entrañables de nuestros clientes. Así que, cuando nos visites, sabrás que no solo estás eligiendo flores, estás compartiendo momentos que perdurarán en el tiempo. </p>";
+			echo "<p class='parrafo'>¡Bienvenido a nuestra florería en Guadalajara! Nuestra pasión por las flores va más allá del simple arte de crear arreglos: cada ramo es un testimonio de una historia única.</p><p class='parrafo'>Un día, una joven llamada Ana vino a nuestra tienda buscando flores para celebrar el aniversario de sus abuelos. Nos contó cómo, hace 50 años, su abuelo solía llevar a su abuela un ramo de girasoles cada semana.</p><p class='parrafo'>Nos inspiró tanto la ternura de su historia que creamos un arreglo especial con girasoles, margaritas y lirios, simbolizando el amor duradero y la belleza de los recuerdos compartidos. Desde ese momento, nos comprometimos a ser parte de las historias más bellas y entrañables de nuestros clientes. Así que, cuando nos visites, sabrás que no solo estás eligiendo flores, estás compartiendo momentos que perdurarán en el tiempo. </p>";
+			echo "<p class='parrafo'><span> ¿Buscas flores frescas y hermosas para alegrar tu día o sorprender a alguien especial? </span>Aquí somos tu florería de confianza en Guadalajara.</p>";
+			echo "<p class='parrafo'>Ofrecemos una amplia variedad de flores frescas, arreglos florales personalizados y regalos para todas las ocasiones.</p>";
 			}
 		?>
 	</section>
